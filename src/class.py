5b7426bf6f093e9b6be3ec0fa7424babe0d5ae59
Louis Weyland <weylandlouis@gmail.com>
2020-01-10 17:02:52 +0100

    Actually delete class.py

--- src/class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-"""
-Created on Wed Jan  8 15:30:03 2020
-This code was implemented by Louis Weyland & Robin van den Berg, Hildebert Mouilé & Wiebe Jelsma
-
-"""
