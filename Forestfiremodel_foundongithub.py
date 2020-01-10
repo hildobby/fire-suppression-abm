73a43888ecb0488ed43471c16ef1d2aceb2a4878
robin <rvdb7345@gmail.com>
2020-01-10 12:27:09 +0100

    added firetrucks to the example

--- Forestfiremodel_foundongithub.py
+++ Forestfiremodel_foundongithub.py
@@ -6,11 +6,104 @@ import matplotlib.pyplot as plt
 
 from mesa import Model, Agent
 from mesa.time import RandomActivation
-from mesa.space import Grid
+from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 from mesa.batchrunner import BatchRunner
 
 
+# defines the model
+class ForestFire(Model):
+    '''
+    Simple Forest Fire model.
+    '''
+
+    def __init__(self, height, width, density):
+        '''
+        Create a new forest fire model.
+
+        Args:
+            height, width: The size of the grid to model
+            density: What fraction of grid cells have a tree in them.
+        '''
+        # Initialize model parameters
+        self.height = height
+        self.width = width
+        self.density = density
+
+        self.n_agents = 0
+        self.agents = []
+
+        # Set up model objects
+        self.schedule = RandomActivation(self)
+        self.grid = MultiGrid(height, width, torus=False)
+        self.dc = DataCollector({"Fine": lambda m: self.count_type(m, "Fine"),
+                                 "On Fire": lambda m: self.count_type(m, "On Fire"),
+                                 "Burned Out": lambda m: self.count_type(m, "Burned Out")})
+
+        # Place a tree in each cell with Prob = density
+        for x in range(self.width):
+            for y in range(self.height):
+                if random.random() < self.density:
+                    # Create a tree
+                    new_tree = TreeCell(self, (x, y))
+                    # Set all trees in the first column on fire.
+                    if x == 0:
+                        new_tree.condition = "On Fire"
+                    self.grid[y][x] = new_tree
+                    self.schedule.add(new_tree)
+        self.running = True
+
+    def step(self):
+        '''
+        Advance the model by one step.
+        '''
+        self.schedule.step()
+        for agent in list(self.agents):
+            agent.step()
+        self.dc.collect(self)
+        # Halt if no more fire
+        if self.count_type(self, "On Fire") == 0:
+            self.running = False
+
+    @staticmethod
+    def count_type(model, tree_condition):
+        '''
+        Helper method to count trees in a given condition in a given model.
+        '''
+        count = 0
+        for tree in model.schedule.agents:
+            if tree.condition == tree_condition:
+                count += 1
+        return count
+
+    def new_agent(self, agent_type, pos):
+        '''
+        Method that enables us to add agents of a given type.
+        '''
+        self.n_agents += 1
+
+        # Create a new agent of the given type
+        new_agent = agent_type(self.n_agents, self, pos)
+
+        # Place the agent on the grid
+        self.grid.place_agent(new_agent, pos)
+
+        # And add the agent to the model so we can track it
+        self.agents.append(new_agent)
+
+    def remove_agent(self, agent):
+        '''
+        Method that enables us to remove passed agents.
+        '''
+        self.n_agents -= 1
+
+        # Remove agent from grid
+        self.grid.remove_agent(agent)
+
+        # Remove agent from model
+        self.agents.remove(agent)
+
+# Defines the tree agents
 class TreeCell(Agent):
     '''
     A tree cell.
@@ -46,65 +139,43 @@ class TreeCell(Agent):
                     neighbor.condition = "On Fire"
             self.condition = "Burned Out"
 
+# defines a random walker class
+class Walker(Agent):
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model)
 
-class ForestFire(Model):
-    '''
-    Simple Forest Fire model.
-    '''
+        self.pos = pos
 
-    def __init__(self, height, width, density):
+    def random_move(self):
         '''
-        Create a new forest fire model.
-
-        Args:
-            height, width: The size of the grid to model
-            density: What fraction of grid cells have a tree in them.
+        This method should get the neighbouring cells (Moore's neighbourhood), select one, and move the agent to this cell.
         '''
-        # Initialize model parameters
-        self.height = height
-        self.width = width
-        self.density = density
+        cell_list = self.model.grid.get_neighborhood(self.pos, moore=True)
+        self.model.grid.move_agent(self, cell_list[random.randint(0, len(cell_list)-1)])
 
-        # Set up model objects
-        self.schedule = RandomActivation(self)
-        self.grid = Grid(height, width, torus=False)
-        self.dc = DataCollector({"Fine": lambda m: self.count_type(m, "Fine"),
-                                 "On Fire": lambda m: self.count_type(m, "On Fire"),
-                                 "Burned Out": lambda m: self.count_type(m, "Burned Out")})
+    def guided_move(self):
+        cell_list = self.model.grid.get_neighborhood(self.pos, moore=True, radius=3)
+        neighbors_list = self.model.grid.get_neighbors(self.pos, moore=True, radius=3)
 
-        # Place a tree in each cell with Prob = density
-        for x in range(self.width):
-            for y in range(self.height):
-                if random.random() < self.density:
-                    # Create a tree
-                    new_tree = TreeCell(self, (x, y))
-                    # Set all trees in the first column on fire.
-                    if x == 0:
-                        new_tree.condition = "On Fire"
-                    self.grid[y][x] = new_tree
-                    self.schedule.add(new_tree)
-        self.running = True
+
+
+class Firetruck(Walker):
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model, pos)
 
     def step(self):
         '''
-        Advance the model by one step.
+        This method should move the Sheep using the `random_move()` method implemented earlier, then conditionally reproduce.
         '''
-        self.schedule.step()
-        self.dc.collect(self)
-        # Halt if no more fire
-        if self.count_type(self, "On Fire") == 0:
-            self.running = False
+        self.random_move()
 
-    @staticmethod
-    def count_type(model, tree_condition):
-        '''
-        Helper method to count trees in a given condition in a given model.
-        '''
-        count = 0
-        for tree in model.schedule.agents:
-            if tree.condition == tree_condition:
-                count += 1
-        return count
+    def extinguish(self):
+        neighbors_list = self.model.grid.get_neighbors(self.pos, moore=True, radius=1)
+        for tree in neighbors_list:
+            if tree.condition == "On Fire":
+                tree.condition = "Burned Out"
+
+
 
 density = 0.6
 width = 100
