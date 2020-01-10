c2e48c296ebf754ad5ee73039174b552e70b7026
Philippe Nicolau <PhilippeNicolau@wcw-stud-145-109-32-163.wireless.uva.nl>
2020-01-10 14:47:31 +0100

    Added my beautiful name

--- src/class.py
+++ src/class.py
@@ -4,6 +4,6 @@
 
 """
 Created on Wed Jan  8 15:30:03 2020
-This code was implemented by Louis Weyland & Robin van den Berg, Hildebert Mouilé & Wiebe Jelsma
+This code was implemented by Louis Weyland, Robin van den Berg, Hildebert Mouilé, Wiebe Jelsma & Philippe Nicolau
 
 """
