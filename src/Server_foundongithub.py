d58741b60b186a799cc73cc2afd4a34ec5489b13
Travis <h.moulie@gmail.com>
2020-01-14 11:55:51 +0000

    PEP-8 Fix [ci skip]

--- src/Server_foundongithub.py
+++ src/Server_foundongithub.py
@@ -55,13 +55,13 @@ model_parameters = {
     'vision': 100,
     'max_speed': 2,
     'density': UserSettableParameter('slider', 'Tree density', 0.65, 0.01, 1.0, 0.01),
-    'temperature': UserSettableParameter('slider', 'Temperature (°C)', 20, 0, 100, 1), #Unused for now
-    'num_firetruck': UserSettableParameter('slider', 'Number of Firetrucks', 30, 0, 100, 1), #Unused for now
+    'temperature': UserSettableParameter('slider', 'Temperature (°C)', 20, 0, 100, 1),  # Unused for now
+    'num_firetruck': UserSettableParameter('slider', 'Number of Firetrucks', 30, 0, 100, 1),  # Unused for now
     'truck_strategy': UserSettableParameter('choice', 'Firetrucks strategy', value='Goes to the biggest fire',
                                             choices=['Goes to the closest fire', 'Goes to the biggest fire',
                                                      'Random movements']),
-    'river_number': UserSettableParameter('slider', 'Number of rivers', 2, 0, 10, 1), #Unused for now
-    'river_width': UserSettableParameter('slider', 'River width', 3, 1, 5, 1), #Unused for now
+    'river_number': UserSettableParameter('slider', 'Number of rivers', 2, 0, 10, 1),  # Unused for now
+    'river_width': UserSettableParameter('slider', 'River width', 3, 1, 5, 1),  # Unused for now
 }
 
 server = ModularServer(ForestFire, [canvas_element, tree_chart, extinguished_chart], "Forest Fire", model_parameters)
