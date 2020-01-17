9004eb109996a5a07a3ab3b3b73541bcdf2307b0
Travis <h.moulie@gmail.com>
2020-01-17 14:41:22 +0000

    PEP-8 Fix

--- src/Server_foundongithub.py
+++ src/Server_foundongithub.py
@@ -8,6 +8,7 @@ from Forestfiremodel_foundongithub import ForestFire
 from agent import *
 from random import randint
 
+
 def forest_fire_portrayal(agent):
     portrayal = {"w": 1,
                  "h": 1,
@@ -52,7 +53,7 @@ extinguished_chart = ChartModule([{"Label": "Extinguished", "Color": "blue"}],
 model_parameters = {
     'height': 100,
     'width': 100,
-    #'wind': (3, 4),
+    # 'wind': (3, 4),
     'vision': 100,
     'max_speed': 2,
     'text_environment': UserSettableParameter('static_text', value='Environment Generation Settings'),
@@ -65,9 +66,9 @@ model_parameters = {
                                             choices=['Goes to the closest fire', 'Goes to the biggest fire',
                                                      'Random movements']),
     'text_settings': UserSettableParameter('static_text', value='Wind Settings'),
-    'wind_strength': UserSettableParameter('slider', 'Wind strength',0.45, 0, 0.5, 0.01),
+    'wind_strength': UserSettableParameter('slider', 'Wind strength', 0.45, 0, 0.5, 0.01),
     'wind_dir': UserSettableParameter('choice', 'Wind Direction', value=('N'),
-                                            choices=['N','NE','E','SE','S','SW','W','NW']),
+                                      choices=['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']),
 
 
     'text_settings': UserSettableParameter('static_text', value='Other Settings'),
