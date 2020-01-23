5c6568eceba53e5a9985f60b47e843fcd1fa3ad7
Louis Weyland <weylandlouis@gmail.com>
2020-01-23 22:34:07 +0100

    - Hopefully it works now ! And why the fuck does the import thing forestfiremodel keep changing !!!!!

--- src/sensitivity_analysis/ofat_sa_plot.py
+++ src/sensitivity_analysis/ofat_sa_plot.py
@@ -78,33 +78,31 @@ for var in list_var:
     for i in range(1, 4):
         ax[0].plot(
             globals()['x_{}_%s'.format(var) % i], globals()[('y_{}_on_fire_%s'.format(var) % i)], label=i)
-        ax[0].fill_between(globals()['x_{}_%s'.format(var) % i],
-                           globals()[('y_{}_on_fire_%s'.format(var) % i)] -
-                           globals()['err_{}_fire_%s'.format(var) % i],
-                           globals()[('y_{}_on_fire_%s'.format(var) % i)] +
-                           globals()['err_{}_fire_%s'.format(var) % i], alpha=0.7)
+        ax[0].fill_between(
+            globals()['x_{}_%s'.format(var) % i],
+            globals()[('y_{}_on_fire_%s'.format(var) % i)] - globals()['err_{}_fire_%s'.format(var) % i],
+            globals()[('y_{}_on_fire_%s'.format(var) % i)] + globals()['err_{}_fire_%s'.format(var) % i], alpha=0.7)
         ax[0].set_xlabel(var)
         ax[0].set_ylabel("On Fire")
         ax[0].legend()
 
         ax[1].plot(
             globals()['x_{}_%s'.format(var) % i], globals()[('y_{}_step_%s'.format(var) % i)], label=i)
-        ax[1].fill_between(globals()['x_{}_%s'.format(var) % i],
-                           globals()[('y_{}_step_%s'.format(var) % i)] -
-                           globals()['err_{}_step_%s'.format(var) % i],
-                           globals()[('y_{}_step_%s'.format(var) % i)] +
-                           globals()['err_{}_step_%s'.format(var) % i], alpha=0.7)
+        ax[1].fill_between(
+            globals()['x_{}_%s'.format(var) % i],
+            globals()[('y_{}_step_%s'.format(var) % i)] - globals()['err_{}_step_%s'.format(var) % i],
+            globals()[('y_{}_step_%s'.format(var) % i)] + globals()['err_{}_step_%s'.format(var) % i], alpha=0.7)
         ax[1].set_xlabel(var)
         ax[1].set_ylabel("Step")
         ax[1].legend()
 
         ax[2].plot(
             globals()['x_{}_%s'.format(var) % i], globals()[('y_{}_extinguish_%s'.format(var) % i)], label=i)
-        ax[2].fill_between(globals()['x_{}_%s'.format(var) % i],
-                           globals()[('y_{}_extinguish_%s'.format(var) % i)] -
-                           globals()['err_{}_extinguish_%s'.format(var) % i],
-                           globals()[('y_{}_extinguish_%s'.format(var) % i)] +
-                           globals()['err_{}_extinguish_%s'.format(var) % i], alpha=0.7)
+        ax[2].fill_between(
+            globals()['x_{}_%s'.format(var) % i],
+            globals()[('y_{}_extinguish_%s'.format(var) % i)] - globals()['err_{}_extinguish_%s'.format(var) % i],
+            globals()[('y_{}_extinguish_%s'.format(var) % i)] + globals()['err_{}_extinguish_%s'.format(var) % i],
+            alpha=0.7)
         ax[2].set_xlabel(var)
         ax[2].set_ylabel("Extinguished")
         ax[2].legend()
