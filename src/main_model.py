8dfa467865f35e4aa24e9d8d8b4e091b594ac50c
robin <rvdb7345@gmail.com>
2020-02-07 10:57:46 +0100

    Commented the main_model.py and firetruck.py (apart from the indirect method). Also added/updated some of the data.

--- src/main_model.py
+++ src/main_model.py
@@ -25,8 +25,25 @@ sys.path.append('../')
 
 # Creating and defining the model with all of its parameters
 class ForestFire(Model):
+
     '''
-    Simple Forest Fire model.
+    Create a forest fire model with fire fighting agents that try to extinguish the fire
+
+    Args:
+        height, width: The size of the grid to model;
+        density: What fraction of grid cells have a tree in them;
+        temperature: influences the number of spontaneous fires;
+        truck_strategy: the tactic that firetrucks will adhere to;
+        river_width: in the case that a river is present, what is its width;
+        break_width: in the case that a pre-made fire break is present, what is its width;
+        random_fires: boolean indicating whether spontaneous fires are present;
+        vision: the distance fire fighting agents can look around them;
+        truck_max_speed: the max speed with which firetruck agents can move around (grid cells/step);
+        wind_strength: the speed with which the wind moves;
+        wind_dir: string specifying the direction of the fire;
+        sparse_ratio: the fraction of the vegetation that is sparse instead of dense;
+        steps_to_extinguishment: number of steps it takes for firetrucks to extinguish a burning cell;
+        randomly_placed: indicates whether the firetrucks are placed randomly over the grid, or equispaced on the rim
     '''
 
     def __init__(
@@ -48,13 +65,7 @@ class ForestFire(Model):
             steps_to_extinguishment,
             randomly_placed):
         super().__init__()
-        '''
-        Create a new forest fire model.
 
-        Args:
-            height, width: The size of the grid to model
-            density: What fraction of grid cells have a tree in them.
-        '''
         # Initializing model parameters
         self.height = height
         self.width = width
@@ -86,26 +97,25 @@ class ForestFire(Model):
         self.schedule = RandomActivation(self)
         self.current_step = 0
 
-        # Seting the wind
-        self.wind = wind_strength
-        self.wind_dir = wind_dir
-
         # Translating the wind_dir string into a vector
         wind_vector = {"\u2B06  North": (0, 1), "\u2197 North/East": (1, 1), "\u27A1 East": (1, 0),
                        "\u2198 South/East": (1, -1), "\u2B07 South": (0, -1), "\u2199 South/West": (-1, -1),
                        "\u2B05 West": (-1, 0), "\u2196 North/West": (-1, 1)}
-        self.wind_dir = wind_vector[self.wind_dir]
+        self.wind_dir = wind_vector[wind_dir]
+        self.wind_strength = wind_strength
+
 
         # Creating the 2D grid
         self.grid = MultiGrid(height, width, torus=False)
 
+        # Create rivers and fire break if applicable
         self.init_river()
         self.init_break(self.break_size)
 
-        # agent_reporters={TreeCell: {"Life bar": "life_bar"}})
-
+        # Create the vegetation agents
         self.init_vegetation(TreeCell, self.initial_tree)
 
+        # add the agents to the vegetation specific schedule and the overal schedule
         for i in range(len(self.agents)):
             self.schedule_TreeCell.add(self.agents[i])
             self.schedule.add(self.agents[i])
@@ -115,8 +125,13 @@ class ForestFire(Model):
         self.num_firetruck = num_firetruck
         self.truck_strategy = truck_strategy
 
+        # initialize the population of firefighters
         self.init_firefighters(Firetruck, num_firetruck, truck_strategy, vision, truck_max_speed, randomly_placed)
+
+        '''
+        rain is currently not implemented realistically, however after future development it could initialised here
         # self.init_rain()
+        '''
 
         # Initialise fire in the middle if possible otherwise random
         self.agents[0].condition = "On Fire"
@@ -136,15 +151,15 @@ class ForestFire(Model):
                                     "Extinguished": lambda m: self.count_extinguished_fires(m)
                                 },
 
-                                # tables={"Life bar": "life_bar", "Burning rate": "burning_rate"},
-
+                                # the data collector was modified to extract different data from different agents
                                 agent_reporters={TreeCell: {"Life bar": "life_bar", "Burning rate": "burning_rate"},
                                                  Firetruck: {"Condition": "condition"}})
 
+        # starting the simulation and collecting the data
         self.running = True
         self.dc.collect(self, [TreeCell, Firetruck])
-        self.wind_strength = wind_strength
 
+        # presetting the indirect attack boundary
         self.buffer_x_min = int((self.init_fire_pos[0]) - 30)
         self.buffer_x_max = int((self.init_fire_pos[0]) + 30)
         self.buffer_y_min = int((self.init_fire_pos[1]) - 30)
@@ -159,7 +174,7 @@ class ForestFire(Model):
         if self.river_width == 0:
             pass
         else:
-            # initiating the river offgrid
+            # initiating the river off-grid
             x = -1
             y_init = random.randrange(self.height - 1)
 
@@ -263,9 +278,16 @@ class ForestFire(Model):
 
     def init_firefighters(self, agent_type, num_firetruck,
                           truck_strategy, vision, truck_max_speed, randomly_placed):
+        '''
+        Initialises the fire fighters
+        randomly_placed: if True --> places the firetrucks randomly over the grid.
+        If False it places the firetrucks equispaced on the rim of the grid.
+        '''
+
         if num_firetruck > 0:
+
+            # Places the firetrucks on the edge of the grid with equal spacing
             if randomly_placed:
-                # Places the firetrucks on the edge of the grib with equal spacing
                 init_positions = self.equal_spread()
                 for i in range(num_firetruck):
                     my_pos = init_positions.pop()
@@ -273,12 +295,15 @@ class ForestFire(Model):
                         Firetruck, my_pos, truck_strategy, vision, truck_max_speed)
                     self.schedule_FireTruck.add(firetruck)
                     self.schedule.add(firetruck)
-                    self.firefighters_lists.append(firetruck)            
+                    self.firefighters_lists.append(firetruck)
+
+            # Places the firetrucks randomly on the grid
             else:
-                # Places the firetrucks randomly on the grid
                 for i in range(num_firetruck):
                     x = random.randrange(self.width)
                     y = random.randrange(self.height)
+
+                    # make sure fire fighting agents are not placed in a river
                     while self.grid.get_cell_list_contents((x, y)):
                         if isinstance(self.grid.get_cell_list_contents(
                                 (x, y))[0], RiverCell):
@@ -286,6 +311,7 @@ class ForestFire(Model):
                             y = random.randrange(self.height)
                         else:
                             break
+
                     firetruck = self.new_firetruck(
                         Firetruck, (x, y), truck_strategy, vision, truck_max_speed)
                     self.schedule_FireTruck.add(firetruck)
@@ -307,12 +333,16 @@ class ForestFire(Model):
         '''
         Advance the model by one step.
         '''
+
+        # progress the fire spread by a step
         self.trees_on_fire = 0
         self.schedule_TreeCell.step()
 
+        # save all burning trees for easy search
         self.tree_list = self.list_tree_by_type(self, "On Fire")
-        if len(self.tree_list) > 0:
 
+        # if using optimized method, produce a matrix with the distances between the firetrucks and the burning veg
+        if len(self.tree_list) > 0:
             if (self.truck_strategy == "Optimized closest"):
                 self.assigned_list = self.assign_closest(
                     self.compute_distances(self.tree_list,
@@ -328,11 +358,14 @@ class ForestFire(Model):
                     self.compute_distances(self.tree_list, self.firefighters_lists),
                     self.tree_list)
 
+            # progress the firetrucks by one step
             self.schedule_FireTruck.step()
 
-        self.dc.collect(self, [TreeCell, Firetruck])
+        # collect data
+        self.dc.collect(self, [TreeCell, Firetruck]) # because of modified dc, now the agents need to be specified
         self.current_step += 1
 
+        # if spontaneous fires are turned on, check whether one ignites in this step
         if self.random_fires:
             randtree = int(random.random() * len(self.agents))
             if self.agents[randtree].condition == "Fine":
@@ -345,6 +378,9 @@ class ForestFire(Model):
 
     @staticmethod
     def randomfire(self, randtree):
+        '''
+        Possibly ignites a new fire, chance depending on the temperature
+        '''
         if (random.random() < (math.exp(self.temperature / 15) / 300.0)):
             self.agents[randtree].condition = "On Fire"
 
@@ -360,6 +396,7 @@ class ForestFire(Model):
         return count
 
     def compute_distances(self, tree_list, truck_list):
+        '''Computes the distances between the firetrucks and the burning vegetation'''
         distances = [[0 for x in range(len(truck_list))] for y in range(len(tree_list))]
         for i in range(len(tree_list)):
             for j in range(len(truck_list)):
@@ -368,20 +405,31 @@ class ForestFire(Model):
         return distances
 
     def assign_closest(self, matrix, tree_list):
+        '''Uses the matrix produces by compute_distances() to assign the firetrucks to the closest burnign vegetation'''
         assigned_trucks = [0 for x in range(self.num_firetruck)]
+
         ratio = Walker.firefighters_tree_ratio(self, self.num_firetruck, len(tree_list))
+
+        # assign firetrucks to the closest fires
         matrix = np.asarray(matrix, dtype=int)
         while 0 in assigned_trucks:
             curr_smallest_pos = np.unravel_index(np.argmin(matrix, axis=None), matrix.shape)
+
+            # if there is a surplus of firetrucks, allow them to go to the same fire
             if assigned_trucks[curr_smallest_pos[1]] == 0 and tree_list[curr_smallest_pos[0]].trees_claimed < ratio:
                 assigned_trucks[curr_smallest_pos[1]] = tree_list[curr_smallest_pos[0]]
+
                 tree_list[curr_smallest_pos[0]].trees_claimed += 1
             matrix[curr_smallest_pos] = 100000
         return assigned_trucks
 
     def assign_parallel(self, matrix, tree_list):
+        '''Uses compute_distances() to carry out the parallel attack'''
         assigned_trucks = [0 for x in range(self.num_firetruck)]
+
+        # if there is a surplus of firetrucks, allow them to go to the same fire
         ratio = Walker.firefighters_tree_ratio(self, self.num_firetruck, len(tree_list))
+
         matrix = np.asarray(matrix, dtype=int)
         for i in range(len(matrix[0])):
             curr_best = [matrix[0][i], tree_list[0].life_bar, 0]
@@ -465,7 +513,7 @@ class ForestFire(Model):
     def new_firetruck(self, agent_type, pos, truck_strategy,
                       vision, truck_max_speed):
         '''
-        Method that enables us to add agents of a given type.
+        Method that enables us to add a fire agent.
         '''
         self.n_agents += 1
 
